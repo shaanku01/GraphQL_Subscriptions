fc7b9ee674ee28335fd196016761ba20842e28e9
shaanku01 <shankersai.gr@zemosolabs.com>
2022-03-24 17:25:28 +0530

    Final commit

---
+++ client/src/graphql/client.js
@@ -0,0 +1,39 @@
+import {
+  ApolloClient, ApolloLink, HttpLink, InMemoryCache , split
+} from 'apollo-boost';
+
+import {WebSocketLink, webSocketLink} from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { getAccessToken } from '../auth';
+
+const httpUrl = 'http://localhost:9000/graphql';
+const wsUrl = 'http://localhost:9000/graphql';
+
+const httpLink = ApolloLink.from([
+  new ApolloLink((operation, forward) => {
+    const token = getAccessToken();
+    if (token) {
+      operation.setContext({headers: {'authorization': `Bearer ${token}`}});
+    }
+    return forward(operation);
+  }),
+  new HttpLink({uri: httpUrl})
+]);
+
+const wsLink = new WebSocketLink({uri:wsUrl,options:{
+  lazy:true,
+  reconnect:true
+}});
+
+function isSubscription(operation){
+  const definition = getMainDefinition(operation.query);
+  return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+}
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: split(isSubscription,wsLink,httpLink),
+  defaultOptions: {query: {fetchPolicy: 'no-cache'}}
+});
+
+export default client;
